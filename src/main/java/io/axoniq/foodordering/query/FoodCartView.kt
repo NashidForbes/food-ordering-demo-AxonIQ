273ff8ecf3ea92aced697fa7f09ff0dda5e34e49
NashidForbes <NashidForbes@biohumans.com>
2023-06-03 23:48:58 -0400

    Creating FoodCartView model and the FoodCartProjector which has eventhandlers and queryhandler for events and query objects.

---
+++ src/main/java/io/axoniq/foodordering/query/FoodCartView.kt
@@ -0,0 +1,18 @@
+package io.axoniq.foodordering.query
+
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
+import javax.persistence.ElementCollection
+import javax.persistence.Entity
+import javax.persistence.Id
+
+
+@Entity
+data class FoodCartView(
+    @Id val foodCartId: UUID,
+    @ElementCollection val products: Map<UUID, Int>
+) {
+    constructor() : this(UUID.randomUUID(), emptyMap())
+}
+
+interface FoodCartViewRepository : JpaRepository<FoodCartView, UUID>
