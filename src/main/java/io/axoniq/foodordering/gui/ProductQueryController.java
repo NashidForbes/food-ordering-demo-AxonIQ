4240bf85f2cb9715cab4aa1dec3615e0cc294129
NashidForbes <NashidForbes@biohumans.com>
2023-06-12 10:27:10 -0400

    Remove ProductView from ProductQueryController.

--- src/main/java/io/axoniq/foodordering/gui/ProductQueryController.java
+++ src/main/java/io/axoniq/foodordering/gui/ProductQueryController.java
@@ -1,14 +1,16 @@
 package io.axoniq.foodordering.gui;
 
 import io.axoniq.foodordering.coreapi.FindProductQuery;
-import io.axoniq.foodordering.query.ProductView;
+import io.axoniq.foodordering.query.model.ProductRestModel;
 import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
@@ -25,7 +27,7 @@ public class ProductQueryController {
     }
 
     @GetMapping("/select/{productId}")
-    public CompletableFuture<ProductView> selectProduct(@PathVariable("productId") UUID productId) {
-        return this.queryGateway.query(new FindProductQuery(productId), ProductView.class);
+    public CompletableFuture<List<ProductRestModel>> selectProduct(@PathVariable("productId") UUID productId) {
+        return this.queryGateway.query(new FindProductQuery(productId), ResponseTypes.multipleInstancesOf(ProductRestModel.class));
     }
 }
