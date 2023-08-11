b9ecf73f5b2e44f53230751cf9286fbb98abbfc2
NashidForbes <NashidForbes@biohumans.com>
2023-08-11 01:40:24 -0400

    Kubernetized food-ordering-products and orders-service.  Including application property files for use in kubernetes cluster.

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation:3.1.0")
     testImplementation("junit:junit:4.13.1")
     implementation("org.postgresql:postgresql:42.6.0")
-    runtimeOnly("org.hsqldb:hsqldb:2.4.1")
     implementation("com.h2database:h2")
     implementation(project(":core"))
     implementation("org.apache.commons:commons-lang3:3.12.0")
